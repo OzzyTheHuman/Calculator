551c78ccb07cbe4ff8aebf7f67ba3e296772d8b2
OzzyTheHuman <151674182+OzzyTheHuman@users.noreply.github.com>
2025-02-23 11:03:05 +0100

    Update program to v3.0

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
 using System.Dynamic;
 using System.Text;
 
@@ -7,6 +9,36 @@ namespace Calculator
 {
     internal class Program
     {
+        public static void SetClipboard(string text) //Windows system utility for copying to clipboard
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.FileName = "clip";
+                processStartInfo.Arguments = "";
+                processStartInfo.RedirectStandardInput = true;
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.CreateNoWindow = true;
+
+                Process process = new Process();
+                process.StartInfo = processStartInfo;
+                process.Start();
+
+                process.StandardInput.WriteLine(text);
+                process.StandardInput.Close();
+
+                process.WaitForExit();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("    - Skopiowano do schowka -");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"    - Błąd kopiowania do schowka: {ex.Message} -");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
         static void Main(string[] args)
         {
             Console.InputEncoding = Encoding.UTF8;
@@ -21,62 +53,86 @@ namespace Calculator
             while (true)
             {
                 decimal value1 = GetNumber();
-                string operation1 = GetOperation();
-                decimal value2;
 
-                decimal result;
-                if (operation1 == "√" || operation1 == "sqrt")
+                while (true)
                 {
-                    Console.CursorTop--;
-                    Console.WriteLine("\t     √   ");
-
-                    while (true)
+                    string operation1 = GetOperation();
+                    if (operation1 == "M")
                     {
-                        try
-                        {
-                            result = (decimal)Math.Sqrt((double)value1);
-                            break;
-                        }
-                        catch (OverflowException)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("    - Błąd pierwiastkowania. Podaj inną liczbę -");
-                            Console.ForegroundColor = ConsoleColor.White;
+                        SetClipboard(value1.ToString());
+                        continue;
+                    }
+                    decimal value2;
 
-                            value1 = GetNumber();
+                    decimal result;
+                    if (operation1 == "√" || operation1 == "sqrt")
+                    {
+                        Console.CursorTop--;
+                        Console.WriteLine("\t     √   ");
+
+                        while (true)
+                        {
+                            try
+                            {
+                                result = (decimal)Math.Sqrt((double)value1);
+                                break;
+                            }
+                            catch (OverflowException)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("    - Błąd pierwiastkowania. Podaj inną liczbę -");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+                                value1 = GetNumber();
+                            }
                         }
                     }
-                }
-                else if (operation1 == "/" || operation1 == "÷")
-                {
-                    Console.CursorTop--;
-                    Console.WriteLine("\t     ÷   ");
-                    Console.CursorTop--;
-                    value2 = GetPercentage(out bool isPercentage);
-
-                    if (isPercentage)
+                    else if (operation1 == "C")
                     {
-                        value2 = value1 * (value2 / 100);
-                        result = GetResult(value1, value2, operation1);
+                        Console.WriteLine("\n---------------------------------------------------------------\n");
+                        break;
+                    }
+                    else if (operation1 == "!")
+                    {
+                        result = value1 * (-1);
+                    }
+                    else if (operation1 == "/" || operation1 == "÷")
+                    {
+                        Console.CursorTop--;
+                        Console.WriteLine("\t     ÷   ");
+                        Console.CursorTop--;
+                        value2 = GetPercentage(out bool isPercentage);
+
+                        if (isPercentage)
+                        {
+                            result = value1 * (value2 / 100);
+                        }
+                        else
+                        {
+                            result = GetResult(value1, value2, operation1);
+                        }
                     }
                     else
                     {
-                        result = GetResult(value1, value2, operation1);
-                    }
-                }
-                else
-                {
-                    Console.CursorTop--;
-                    value2 = GetPercentage(out bool isPercentage);
+                        Console.CursorTop--;
+                        value2 = GetPercentage(out bool isPercentage);
 
-                    if (isPercentage)
-                    {
-                        value2 = value1 * (value2 / 100);
-                        result = GetResult(value1, value2, operation1);
+                        if (isPercentage)
+                        {
+                            result = value1 * (value2 / 100);
+                        }
+                        else
+                        {
+                            result = GetResult(value1, value2, operation1);
+                        }
                     }
-                    result = GetResult(value1, value2, operation1);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"\t\t\b\b\b=  {result}");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    value1 = result;
                 }
-                GetEndingWords(result);
+
             }
         }
         public static void GetStartingWords()
@@ -94,7 +150,7 @@ namespace Calculator
             Console.Write("> Obsługiwane operacje ");
 
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("|+| |-| |*| |÷| |√| |%| |^|\n");
+            Console.Write("|+| |-| |*| |÷| |√| |%| |^| |!| |C|\n");
 
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write("> Aby obliczyć pierwiastek kwadratowy wystarczy ");
@@ -106,17 +162,39 @@ namespace Calculator
             Console.Write("lub ");
 
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("'sqrt'\n");
+            Console.WriteLine("'sqrt'");
 
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("> Aby wkleić tekst należy wcisnąć prawy przycisk myszy");
-            Console.Write("> ");
+            Console.Write("> Aby zmienić znak liczby należy wprowadzić ");
 
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("'CTRL + C' ");
+            Console.WriteLine("'!'");
 
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("aby zakończyć");
+            Console.Write("> Aby zakończyć ciąg operacji należy wprowadzić ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("'C'");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("> Aby skopiować tekst należy wprowadzić ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("'M'");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("> Aby wkleić tekst należy wcisnąć ");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("prawy przycisk myszy");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("> Aby zakończyć");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" 'CTRL + C'");
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
             Console.WriteLine("1. Podaj liczbę + ENTER");
             Console.WriteLine("2. Podaj operację + ENTER");
@@ -156,7 +234,7 @@ namespace Calculator
             {
                 Console.Write("\t\t\b\b\b");
                 string input = Console.ReadLine()!;
-                switch (input)
+                switch (input.ToUpper())
                 {
                     case "+":
                         return "+";
@@ -170,10 +248,14 @@ namespace Calculator
                     case "√":
                     case "sqrt":
                         return "√";
-                    case "%":
-                        return "%";
                     case "^":
                         return "^";
+                    case "C":
+                        return "C";
+                    case "!":
+                        return "!";
+                    case "M":
+                        return "M";
                     default:
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine($"    - Nieobsługiwana operacja, spróbuj ponownie -");
@@ -199,12 +281,11 @@ namespace Calculator
                         case "/":
                         case "÷":
                             return checked(value1 / value2);
-                        case "%":
-                            return checked(value1 * value2 / 100);
                         case "^":
                             double value1d = Convert.ToDouble(value1);
                             double value2d = Convert.ToDouble(value2);
                             return checked((decimal)Math.Pow(value1d, value2d));
+
                     }
                 }
                 catch (OverflowException)
--- README.md
+++ README.md
@@ -3,39 +3,15 @@ A console-based calculator project written in C#.<br />This application is devel
 
 
 ## Instalation
-You do not need .NET runtime.<br />Simply unzip newest version from **Releases**, locate **Calculator.exe** file and you are good to go.
-
-## Quick disclaimer how % works
-**Scenario 1: % used as a operation**<br />
-Lets say, you type **55 % 10** How it will be handled ?
-- Get value1 = 55
-- Get operation = "%"
-- Get value2 = 10 ( isPercentage = false )
-- Calculated value2 = 55 * (10 / 100) = 5.5
-
-**Scenario 2: % used in calcultations**<br />
-Lets say, you type **55 * 10%** How it will be handled ?
-- Get value1 = 55
-- Get operation = "*"
-- Get value2 = 10% ( isPercentage = true )
-- Calculated value2 = 55 * (10 / 100) = 5.5
-- Call GetResult(55; 5.5, "*") which will return 55 * 5.5 = 302.5
-
-_Why doesn't this app show 5.5 when I type 55 * 10%?_ Most calculators show 5.5 (which is 10% of 55). But my app shows 302.5 because it calculates 55 * 5.5 instead of calculating 10% of 55. App skips the step of calculating what percentage of the number you want, and goes straight to multiplying by a factor derived from the percentage. It multiplies by that factor immediately. 
-
-Therefore, if you're looking to find a percentage of a number – for example, calculating what _10% of 55 is_ – you should use a % operator.
-
+You do not need .NET runtime.<br />Simply unzip newest version from **Releases**, locate **Calculator.exe** file and you are good to go.<br /> Sometimes windows defender will flag this as a virus, **its a false positive** and you are safe to ignore it.<br /> Check my source code, worst thing you will find is messy and inefficient code.
 
 ## TODO LIST
 
-- [x] Basic arithmetic operations | + | - | * | ÷ |&nbsp;&nbsp; ***v.1.0***
-- [x] More advenced operations | √ | % | ^ |&nbsp;&nbsp; ***v.2.1***
-- [ ] Memory acces 
-    - [ ] Implement | M+&nbsp; | (Add to memory)
-    - [ ] Implement | M-&nbsp; | (Subtract from memory)
-    - [ ] Implement | MRC | (Memory Recall)
-- [ ] Sign change 
+- [x] Basic arithmetic operations | + | - | * | ÷ |&nbsp;&nbsp; ***v1.0***
+- [x] More advenced operations | √ | % | ^ |&nbsp;&nbsp; ***v2.1***
+- [x] Memory acces - | M |&nbsp;&nbsp; ***v3.0***
+- [x] Sign change - | ! |&nbsp;&nbsp; ***v3.0***
 - [ ] Multi-language support 
-    - [x] Polish&nbsp;&nbsp; ***v.1.0***
+    - [x] Polish&nbsp;&nbsp; ***v1.0***
     - [ ] English
 - [ ] Mouse support
